146e9de74d8d3ccb91c9fd46bfaf25512588e37b
Samuel Erb <samuel.erb@me.com>
2017-12-17 15:22:32 +0100

    try

--- config/bundles.php
+++ config/bundles.php
@@ -9,4 +9,5 @@ return [
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
 ];
--- src/Controller/Default_Controller.php
+++ src/Controller/Default_Controller.php
@@ -8,18 +8,17 @@
 
 namespace App\Controller;
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\Routing\Annotation\Route;
 
 class Default_Controller extends Controller
 {
     /**
      * @Route("/", name="start")
      * @Template
-     * @param $name
      */
-    public function index($name)
+    public function index()
     {
     }
 }
